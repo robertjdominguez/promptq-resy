6ee41dd816f410b6fa20a7d65fc8ef5697a3276d
Rob Dominguez <rob.dominguez@hasura.io>
2025-04-12 12:24:40 -0700

    Add ability to get city code

--- maitre_d/connector/booking_logic/functions.ts
+++ maitre_d/connector/booking_logic/functions.ts
@@ -3,6 +3,46 @@ interface Coordinates {
   lon: number;
 }
 
+interface CityCode {
+  code: string;
+}
+
+type ResyCity = {
+  all_cities_header: number;
+  blog_url: string | null;
+  code: string;
+  content_tier: number;
+  country_code: string;
+  country_id: number;
+  country_name: string;
+  has_rga_venues: boolean;
+  id: number;
+  latitude: number;
+  longitude: number;
+  map_center: {
+    latitude: number;
+    longitude: number;
+  };
+  name: string;
+  radius: number;
+  shape_data: any;
+  show_in_app: number;
+  show_in_display: number;
+  show_on_web: number;
+  time_zone: string;
+  url_slug: string;
+  waitlist_max_distance: number;
+  waitlist_travel_mode: string;
+  metadata: {
+    description: string;
+    keywords: string[];
+    all_cities: {
+      header: string;
+      tagline: string;
+    };
+  };
+};
+
 /**
  *
  * This function fetches the coordinates of a city using the OpenCage Geocoding API.
@@ -36,4 +76,25 @@ export async function getLatLon(city: string): Promise<Coordinates> {
     lat: parseFloat(data[0].lat),
     lon: parseFloat(data[0].lon),
   };
+}
+
+/**
+ * This function takes a pair of coordinates, and returns a city code from the Resy API.
+ *
+ * @readonly
+ * @param {Coordinates} coordinates - The coordinates to get the city code for
+ * @returns {Promise<CityCode>} - A promise that resolves to the city code
+ */
+export async function getCityCode(coordinates: Coordinates): Promise<CityCode> {
+  const url = `https://api.resy.com/3/location/config?lat=${coordinates.lat}&long=${coordinates.lon}`;
+
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    console.warn(`City code fetch failed: ${response.statusText}`);
+    return { code: "" };
+  }
+  const data: any = await response.json();
+  const code = data[0].code;
+  return { code: code };
 }
